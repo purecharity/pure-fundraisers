f399e443ac45a8a9e5d1a34f9a46fd1b2b8b0e62
Rafael Dalprá <thedalpra@gmail.com>
2015-06-09 16:27:25 -0300

    Version Bump to test

--- purecharity-wp-fundraisers/purecharity-wp-fundraisers.php
+++ purecharity-wp-fundraisers/purecharity-wp-fundraisers.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Pure Charity Fundraisers
  * Plugin URI:        http://purecharity.com/
  * Description:       Plugin with a collection of shortcodes and template tags to display Pure Charity Fundraisers
- * Version:           1.1.3
+ * Version:           1.1.4
  * Author:            Rafael Dalprá / Pure Charity
  * Author URI:        http://purecharity.com/
  * License:           GPL-2.0+
