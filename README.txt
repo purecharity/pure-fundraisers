51a3931d7d2bebab0ea45687ed218bafd24f09b9
Rafael Dalprá <thedalpra@gmail.com>
2015-08-28 12:43:10 -0300

    Fixes the single view blank space when funding is hidden

--- README.txt
+++ README.txt
@@ -30,6 +30,9 @@ directory take precedence. For example, `/assets/screenshot-1.png` would win ove
 
 == Changelog ==
 
+= 1.1.7 =
+* Minor bugfixes.
+
 = 1.1.6 =
 * Critical bugfix on meta-tags.
 
--- purecharity-wp-fundraisers.php
+++ purecharity-wp-fundraisers.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Pure Charity Fundraisers
  * Plugin URI:        http://purecharity.com/
  * Description:       Plugin with a collection of shortcodes and template tags to display Pure Charity Fundraisers
- * Version:           1.1.6
+ * Version:           1.1.7
  * Author:            Pure Charity
  * Author URI:        http://purecharity.com/
  * License:           GPL-2.0+
@@ -103,8 +103,8 @@ function fr_force_template() {
  *
  * @since    1.1.5
  */
-add_action( 'wp_head', 'set_meta_tags' );
-function set_meta_tags(){
+add_action( 'wp_head', 'set_fr_meta_tags' );
+function set_fr_meta_tags(){
 	if(isset($_GET['slug'])){
 		$base_plugin = new Purecharity_Wp_Base();
 		$fundraiser = $base_plugin->api_call('fundraisers/show?slug='. $_GET['slug'])->fundraiser;
